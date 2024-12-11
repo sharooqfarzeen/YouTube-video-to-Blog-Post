2c2b9b2d4d4a4e3cfd98a8bf5b1e893e4bb38a44
sharooqfarzeen <sharooqfarzeen@gmail.com>
2024-12-11 09:23:23 +0530

    Fixed sqlite 3 error in app.py

--- src/yt_to_blog/app.py
+++ src/yt_to_blog/app.py
@@ -1,7 +1,7 @@
-# # these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Streamlit app
 import io
